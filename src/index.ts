ca02b0395ffb22ff5d6a802085fd9d3866843616
Fadhil <fadhilahdwi.a@gmail.com>
2024-04-30 16:02:43 +0700

    setup husky, eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
