66ee4e5496d4b8589d5ca5df9bbc0b34a637c630
Fadhil <fadhilahdwi.a@gmail.com>
2024-04-30 13:11:23 +0700

    new setup bun

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { app } from './app.js'
-import { config } from './config/config.js'
-import { logger } from './config/logger.js'
-
-app.listen(config.port, () => {
-  logger.info(`Listening to port ${config.port}`)
-})
--- src/routes/v1/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const authRouter = new express.Router()
-authRouter.get('/', (req, res) => { res.send('Express on Fire') })
-
-export {
-  authRouter
-}
